7145a4cc21ff31d9477a5a8fe3c2241cfc2fd836
Phayao Boonon <phayaob@gmail.com>
2017-09-12 21:54:56 +0700

    Add Identify for Infrastructure

---
+++ src/Infrastructure/Identity/AppIdentifyDbContext.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Identity
+{
+    public class AppIdentifyDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppIdentifyDbContext(DbContextOptions<AppIdentifyDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+    }
+}
