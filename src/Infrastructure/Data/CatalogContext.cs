3af9e0d2cb11274a5e2724662c610c05d1d305c7
Phayao Boonon <phayaob@gmail.com>
2017-09-14 23:00:07 +0700

    Update Data for Infrastructure

---
+++ src/Infrastructure/Data/CatalogContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Data
+{
+    class CatalogContext : DbContext
+    {
+        public CatalogContext(DbContextOptions<CatalogContext> options) : base(options)
+        {
+
+        }
+
+
+    }
+}
