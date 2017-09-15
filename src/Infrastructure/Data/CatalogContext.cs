375afe5814fd005d8297fa2a18f176f4585cf1f7
Phayao Boonon <phayaob@gmail.com>
2017-09-15 20:47:06 +0700

    Update CatalogContext

--- src/Infrastructure/Data/CatalogContext.cs
+++ src/Infrastructure/Data/CatalogContext.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Infrastructure.Data
 {
@@ -10,6 +10,81 @@ namespace Infrastructure.Data
         public CatalogContext(DbContextOptions<CatalogContext> options) : base(options)
         {
 
+        }
+
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<CatalogItem> CatalogItems { get; set; }
+        public DbSet<CatalogBrand> CatalogBrands { get; set; }
+        public DbSet<CatalogType> CatalogTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Basket>(ConfigureBasket);
+            builder.Entity<CatalogBrand>(ConfigureCatalogBrand);
+            builder.Entity<CatalogType>(ConfigureCatalogType);
+            builder.Entity<CatalogItem>(ConfigureCatalogItem);
+        }
+
+        private void ConfigureBasket(EntityTypeBuilder<Basket> builder)
+        {
+            var navigation = builder.Metadata.FindNavigation(nameof(Basket.Items));
+            navigation.SetPropertyAccessMode(PropertyAccessMode.Field);
+        }
+
+        private void ConfigureCatalogItem(EntityTypeBuilder<CatalogItem> builder)
+        {
+            builder.ToTable("Catalog");
+
+            builder.Property(ci => ci.Id)
+                .IsRequired();
+
+            builder.Property(ci => ci.Name)
+                .IsRequired(true)
+                .HasMaxLength(50);
+
+            builder.Property(ci => ci.Price)
+                .IsRequired(true);
+
+            builder.Property(ci => ci.PictureUri)
+                .IsRequired(false);
+
+            builder.HasOne(ci => ci.CatalogBrand)
+                .WithMany()
+                .HasForeignKey(ci => ci.CatalogBrandId);
+
+            builder.HasOne(ci => ci.CatalogType)
+                .WithMany()
+                .HasForeignKey(ci => ci.CatalogTypeId);
+            
+        }
+
+        private void ConfigureCatalogBrand(EntityTypeBuilder<CatalogBrand> builder)
+        {
+            builder.ToTable("CatalogBrand");
+
+            builder.HasKey(ci => ci.Id);
+
+            builder.Property(ci => ci.Id)
+                .IsRequired();
+
+            builder.Property(cb => cb.Brand)
+                .IsRequired()
+                .HasMaxLength(100);
+        }
+
+
+        private void ConfigureCatalogType(EntityTypeBuilder<CatalogType> builder)
+        {
+            builder.ToTable("CatalogType");
+
+            builder.HasKey(ci => ci.Id);
+
+            builder.Property(ci => ci.Id)
+                .IsRequired();
+
+            builder.Property(cb => cb.Type)
+                .IsRequired()
+                .HasMaxLength(100);
         }
 
 
